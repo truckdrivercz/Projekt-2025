7b9c33adcec3faff329cdc384c0b4af5ce7d6d8b
truckdrivercz <eda.orvos@seznam.cz>
2025-03-06 15:00:32 +0100

    update

--- res/js/main.js
+++ res/js/main.js
@@ -17,10 +17,27 @@ class Player{
     }
 
     update(keys){
-        if(keys["w"]) this.y -= this.v;
-        if(keys["s"]) this.y += this.v;
-        if(keys["a"]) this.x -= this.v;
-        if(keys["d"]) this.x += this.v;
+        if(keys["w"]){
+            if(this.y - this.v > 0){
+                this.y -= this.v;
+            }
+        }
+        if(keys["s"]){
+            if(this.y + this.h + this.v < canvas.height){
+                this.y += this.v;
+            }
+            
+        }
+        if(keys["a"]){
+            if(this.x - this.v > 0){
+                this.x -= this.v;
+            }
+        }
+        if(keys["d"]){
+            if(this.x + this.h + this.v < canvas.width){
+                this.x += this.v;
+            }
+        }
         if(keys[" "]) {
             this.w -= 2;
             this.h -= 2;
@@ -53,6 +70,14 @@ let keys = {};
 window.addEventListener("keydown", (e) => keys[e.key] = true);
 window.addEventListener("keyup", (e) => keys[e.key] = false);
 
+const testObject = {
+    w: 100,
+    h: 100,
+    x: 200,
+    y: 300,
+    c:"blue"
+}
+
 const gameLoop = () =>{
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
@@ -60,6 +85,17 @@ const gameLoop = () =>{
 
     myPlayer.draw(ctx);
 
+    ctx.fillStyle = testObject.c;
+    ctx.fillRect(testObject.x, testObject.y, testObject.w, testObject.h);
+    checkCollision(myPlayer, testObject);
     requestAnimationFrame(gameLoop);
 }
 requestAnimationFrame(gameLoop);
+
+const checkCollision = (object1, object2) => {
+    if(object1.x + object1.w > object1.x &&
+        object1.x < object2.x + object2.w &&
+        object1.y < object1.h > object2.y){
+        console.log("Collision detecter");
+    }
+}
